fdcdbb0b92c1f8dbb297ef4aae53d9291bdf63ad
Kevin Fields <kfields31@gmail.com>
2023-02-12 14:35:12 -0600

    responsive email link

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import "./HomePage.css";
 
 const HomePage = () => {
   return (
     <div className="page">
       <h3 className="page-header">HTML made a bit easier.</h3>
+      <a 
+        className="homepage-email-link"
+        href="mailto:kfields31@gmail.com?subject=HTML Helper Praise" 
+        target="_blank" 
+        rel="noreferrer"
+      >
+        Email me about this!
+      </a>
     </div>
   )
 }
